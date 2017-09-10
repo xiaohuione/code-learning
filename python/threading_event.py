c9def14c92eee114247080511bbe0a6464349738
xiaohuione <xiaohuione@163.com>
2017-09-11 00:23:12 +0800

    learning thread event

--- python/threading_event.py
+++ python/threading_event.py
@@ -6,9 +6,8 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-10s) %(message)s',)
 
 def worker(event):
-    while not event.is_set():
-        logging.debug('Waiting for redis ready...')
-        event.wait(1)
+    logging.debug('Waiting for redis ready...')
+    event.wait()
     logging.debug('redis ready, and connect to redis server and do some work [%s]', time.ctime())
     time.sleep(1)
     logging.debug('work done')
@@ -18,8 +17,6 @@ if __name__ == '__main__':
     threads = []
     t1 = threading.Thread(target=worker, args=(readis_ready,), name='t1')
     threads.append(t1)
-    t2 = threading.Thread(target=worker, args=(readis_ready,), name='t2')
-    threads.append(t2)
     for t in threads:
         logging.debug("start {0}".format(t.name))
         t.start()
